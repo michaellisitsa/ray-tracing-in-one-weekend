e715f86ce38201ed2e006a628c0ad7f9c3e138e4
Michael Lisitsa <michael.lisitsa@gmail.com>
2024-06-29 20:21:38 +1000

    Add remaining operator overloads

--- src/headers/vec3.h
+++ src/headers/vec3.h
@@ -106,6 +106,43 @@ inline vec3 operator-(const vec3 &u, const vec3 &v)
     return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
 }
 
+inline vec3 operator*(const vec3 &u, const vec3 &v)
+{
+    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+}
+
+inline vec3 operator*(double t, const vec3 &v)
+{
+    return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+}
+
+inline vec3 operator*(const vec3 &v, double t)
+{
+    return t * v;
+}
+
+inline vec3 operator/(const vec3 &v, double t)
+{
+    return (1 / t) * v;
+}
+
+inline double dot(const vec3 &u, const vec3 &v)
+{
+    return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2];
+}
+
+inline vec3 cross(const vec3 &u, const vec3 &v)
+{
+    return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+                u.e[2] * v.e[0] - u.e[0] * v.e[2],
+                u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
+
+inline vec3 unit_vector(const vec3 &v)
+{
+    return v / v.length();
+}
+
 // We want to overload ostream so we can print out vec3 objects
 // without looping over each parameter
 // But does the << do. Is it an append operator the lvalue?
